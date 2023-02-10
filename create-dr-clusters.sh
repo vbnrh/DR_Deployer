280d40d23230d8ece4cceeb3900a1a31396da8ff
Vineet Badrinath <vineetbnath@gmail.com>
2023-02-10 12:56:58 +0530

    Remove creation of vrcs from the script
    Signed-off-by: Vineet Badrinath <vineetbnath@gmail.com>

--- create-dr-clusters.sh
+++ create-dr-clusters.sh
@@ -72,8 +72,6 @@ function create-hub-cluster {
 	kubectl create -f multiclusterhub.yaml
 	label-nodes
 	kubectl create -f deploy-odf.yaml
-	# oc create -f https://raw.githubusercontent.com/red-hat-storage/volume-replication-operator/main/config/crd/bases/replication.storage.openshift.io_volumereplications.yaml
-  # oc create -f https://raw.githubusercontent.com/red-hat-storage/volume-replication-operator/main/config/crd/bases/replication.storage.openshift.io_volumereplicationclasses.yaml
 	sleep 1m
 	kubectl create -f storagesystem.yaml
 	oc create namespace odfmo-system
@@ -88,8 +86,6 @@ function create-spoke-cluster {
 	export KUBECONFIG=${SPOKE_NAME}/auth/kubeconfig
 	label-nodes
 	oc create -f deploy-odf.yaml
-	oc create -f https://raw.githubusercontent.com/red-hat-storage/volume-replication-operator/main/config/crd/bases/replication.storage.openshift.io_volumereplications.yaml
-  oc create -f https://raw.githubusercontent.com/red-hat-storage/volume-replication-operator/main/config/crd/bases/replication.storage.openshift.io_volumereplicationclasses.yaml
 	sleep 1m
 	oc create -f storagesystem.yaml
 }
